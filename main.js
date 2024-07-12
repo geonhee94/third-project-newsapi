cdba6288a91b3327764016b93b7052e08bb95ca3
geonhee <ghkorean@naver.com>
2024-07-12 11:07:41 +0900

    sixth

--- main.js
+++ main.js
@@ -4,39 +4,47 @@ let newsList=[]
 const menus = document.querySelectorAll(".menus button")
 menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
 
+let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${API_KEY}`)
+
+const getNews = async() => {
+    try{
+      const response = await fetch(url);
+      const data = await response.json();
+      if(response.status==200){
+        if(data.articles.length==0){
+          throw new Error("검색어와 일치하는 결과가 없습니다.");
+        }
+        newsList = data.articles;
+        render();
+      }else{
+        throw new Error(data.message)
+      }
+    }catch(error){
+      errorRender(error.message)
+    }
+  
+}
 
 const getLatestNews = async ()=>{
-    const url = new URL(
-        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${API_KEY}`
+     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${API_KEY}`
+        
     ) ;
-    const response = await fetch(url)
-    const data = await response.json()
-    newsList = data.articles
-    render()
+    
+    getNews()
     
 };
 
 
 const getNewsByCategory = async (event)=>{
   const category = event.target.textContent.toLowerCase()
-  console.log("category",category)
-  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
-  const response = await fetch(url)
-  const data = await response.json()
-  
-  newsList = data.articles
-  render()
+   url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
+   getNews()
 }
 
 const searchNews= async ()=>{
   const keyword = document.getElementById("search-input").value
-  console.log("keyword",keyword);
-  const url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
-  const response = await fetch(url)
-  const data = await response.json()
-  
-  newsList = data.articles
-  render()
+   url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
+   getNews()
 }
 
   const openSearchBox = () => {
@@ -69,6 +77,13 @@ const render=()=>{
     )
     .join('');
     document.getElementById("news-board").innerHTML=newsHTML
+}
+
+const errorRender = (errorMessage)=>{
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+  ${errorMessage}
+</div>`
+document.getElementById("news-board").innerHTML=errorHTML
 }
 const openNav = () => {
     document.getElementById("hamburgerNav").style.width = "250px";
